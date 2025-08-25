40beea71d061c93ba0e9c219c1603f5a978f9ea9
ThemLemons <jimmyyo12@gmail.com>
2025-08-25 20:39:30 +1200

    updated chooser

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lemonCsvReader
 A python based program that outlines a CSV file 
+
+## Requirements
+if you decide to use pip to run python programs. 
+```pip install --upgrade pip```
+- This project does the os package to locate the documents location of the computer.
+
+
+```pip install tk```
+- This project does tkinter package to open a ui to let the user choose a csv file.
+
+
+## Installation instruction
+Up coming
+
--- pythonCSVReader.py
+++ pythonCSVReader.py
@@ -1,8 +1,31 @@
 import csv
+import os
+import tkinter as tk
+from tkinter import filedialog
 #Python reading program
 
-with open('data.csv', 'r') as file:
-     csv_read = csv.reader(file)
-     header = next(csv_read)
-     for row in csv_read:
-         print(row)
+#Using this to locate the home directory & documents directory
+home_dir = os.path.expanduser("~")
+docum_dir = os.path.join(home_dir,"Documents")
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename(
+    #file dialog to open the home dir & document directory
+    initialdir=docum_dir,
+    title="select a file",
+    filetypes=(("csv file","*.csv"),("All files","*.*"))
+)
+
+if file_path:
+   print("you have selected file:"+ file_path)
+else:
+   print("No File Selected.")
+
+
+# with open('data.csv', 'r') as file:
+#     csv_read = csv.reader(file)
+#     header = next(csv_read)
+#     for row in csv_read:
+#         print(row)
